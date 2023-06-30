4fb4ceaa7e774ca596a2979a53d53cd24ec9233a
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-06-30 16:26:05 +0300

    UPDATE: added fonts in project

--- app/App.js
+++ app/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { StyleSheet, View,TouchableWithoutFeedback,Keyboard } from "react-native";
+import { useFonts } from 'expo-font';
 
 import RegistrationScreen from "./Screens/RegistrationScreen";
 import LoginScreen from "./Screens/LoginScreen"
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+    'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
     <View style={styles.container}>
       <RegistrationScreen />
